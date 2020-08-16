654c05fd5e901e51cf38a4dbcc4120966f0c6d68
2zqa <25235249+2zqa@users.noreply.github.com>
2020-08-16 19:45:06 +0200

    Update createmod.bat

--- createmod.bat
+++ createmod.bat
@@ -1,5 +1,6 @@
-@REM script not tested, sorry!
-recompile.bat
-reobfuscate.bat
-DEL /F AnnoyanceFix.zip
-zip -r -j AnnoyanceFix.zip reobf/minecraft
+@echo off
+call "recompile.bat" < nul
+call "reobfuscate.bat" < nul
+del /F AnnoyanceFix.zip
+"C:\Program Files\7-Zip\7z.exe" a "AnnoyanceFix.zip" .\reobf\minecraft\*
+pause
